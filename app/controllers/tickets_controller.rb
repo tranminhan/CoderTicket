80c94b498f600440342cd2bfdad656da96734ad9
An Tran Minh <tranminhan.my@gmail.com>
2015-12-22 01:45:39 +0700

    style tickets page

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,11 +1,19 @@
 class TicketsController < ApplicationController
   def new
     @event = Event.find(params[:event_id])
-    # @ticket = Ticket.new
   end
 
   def create
-    
+    # debugger
+    ticket = params[:ticket]
+    @tickets = []
+    ticket.each do |key, value| 
+      if value.to_i > 0 
+        @tickets << { type: TicketType.find(key), qty: value }
+      end 
+    end
+    @total = @tickets.inject(0){ |sum, e| sum += e[:type][:price].to_f }
+    # debugger
   end
 
 end
